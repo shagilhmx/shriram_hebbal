145647d9a110642583cf28b17066bd5f73ae9327
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2023-03-23 12:23:46 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -129,32 +129,6 @@ window.addEventListener(
   false,
 );
 
-function slideUp(target, duration = 400) {
-  target.style.transitionProperty = "height, margin, padding";
-  target.style.transitionDuration = duration + "ms";
-  target.style.boxSizing = "border-box";
-  target.style.height = target.offsetHeight + "px";
-  target.offsetHeight;
-  target.style.overflow = "hidden";
-  target.style.height = 0;
-  target.style.paddingTop = 0;
-  target.style.paddingBottom = 0;
-  target.style.marginTop = 0;
-  target.style.marginBottom = 0;
-  window.setTimeout(() => {
-    target.style.display = "none";
-    target.style.removeProperty("height");
-    target.style.removeProperty("padding-top");
-    target.style.removeProperty("padding-bottom");
-    target.style.removeProperty("margin-top");
-    target.style.removeProperty("margin-bottom");
-    target.style.removeProperty("overflow");
-    target.style.removeProperty("transition-duration");
-    target.style.removeProperty("transition-property");
-    //alert("!");
-  }, duration);
-}
-
 window.addEventListener(
   "load",
   (event) => {
