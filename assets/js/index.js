83c2250dcd7cbdb6de7240cdbb2fabd4be45a1a9
Md Shagil Nizmai <shagilhmx@gmail.com>
2023-04-04 13:49:33 +0000

    Update 3 files

--- assets/js/index.js
+++ assets/js/index.js
@@ -253,7 +253,7 @@ function openApi(event, on) {
       }),
     };
     axios
-      .post("http://api-dcrm-stage.fincity.in/open/opportunity", body)
+      .post("http://api-dcrm-dev.fincity.in/open/opportunity", body)
       .then((res) => {
         if (isOtp) {
           if (on) {
@@ -418,10 +418,7 @@ function detectLocation(e, check) {
         };
 
         axios
-          .post(
-            `http://api-dcrm-stage.fincity.in/open/opportunity/verify`,
-            body
-          )
+          .post(`http://api-dcrm-dev.fincity.in/open/opportunity/verify`, body)
           .then((res) => {
             document.getElementById(
               check ? "detectText" : "detectText1"
@@ -476,7 +473,7 @@ function resendOtp(e, check) {
   e.stopPropagation();
   axios
     .post(
-      `http://api-dcrm-stage.fincity.in/open/opportunity/send-otp?token=${responseData?.data?.token}`
+      `http://api-dcrm-dev.fincity.in/open/opportunity/send-otp?token=${responseData?.data?.token}`
     )
     .then((res) => {
       document.querySelector(check ? "#resendOtp" : "#resendOtp1").innerText =
@@ -498,7 +495,7 @@ function verfiyOtp(e, check) {
     otp: otp,
   };
   axios
-    .post(`http://api-dcrm-stage.fincity.in/open/opportunity/verify`, body)
+    .post(`http://api-dcrm-dev.fincity.in/open/opportunity/verify`, body)
     .then((res) => {
       document.getElementById(
         check ? "otpVerification" : "otpVerification1"
@@ -515,7 +512,7 @@ function verfiyOtp(e, check) {
         if (count === 0) {
           let deviceType = getDeviceType();
           clearInterval(countdown);
-          window.location.href = `http://dcrm-stage.fincity.in/?&user=consumer&device-type=${deviceType}&token=${res?.data?.consumerToken}&isLandingPage=true`;
+          window.location.href = `http://dcrm-dev.fincity.in/?&user=consumer&device-type=${deviceType}&token=${res?.data?.consumerToken}&isLandingPage=true`;
         }
       }, 1000);
     })
